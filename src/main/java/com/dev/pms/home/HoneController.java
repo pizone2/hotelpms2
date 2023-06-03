2b3a62a096d52a4e02e9f7ef773a88d115413166
10000JI <sksk436@naver.com>
2023-06-04 02:05:11 +0900

    setting

---
+++ src/main/java/com/dev/pms/home/HoneController.java
@@ -0,0 +1,12 @@
+package com.dev.pms.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HoneController {
+    @GetMapping("/")
+    public String home() throws  Exception{
+        return "index";
+    }
+}
