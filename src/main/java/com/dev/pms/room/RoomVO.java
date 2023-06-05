09a9115f17ea4a5facc39b9a0c61f7f0a7563215
hee jin kim <jinjinzala@naver.com>
2023-06-05 17:09:05 +0900

    예약 객실타입 룸 VO 추가

--- src/main/java/com/dev/pms/room/RoomVO.java
+++ src/main/java/com/dev/pms/room/RoomVO.java
@@ -1,6 +1,14 @@
 package com.dev.pms.room;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class RoomVO {
 
+    private Long roomNumber;
+    private String roomType;
+    private  String roomName;
 
 }
