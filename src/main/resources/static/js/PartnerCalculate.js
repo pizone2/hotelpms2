defab28c2d2b6a40b1fe9aa7c5fb5f9e122f604f
hee jin kim <jinjinzala@naver.com>
2023-06-19 02:07:17 +0900

    ajax의 호출 주소가 정상으로 들어오는지 확인

--- src/main/resources/static/js/PartnerCalculate.js
+++ src/main/resources/static/js/PartnerCalculate.js
@@ -1,39 +1,43 @@
-$.ajax({
-    url: "/partnerCalculate/receipt?date=2023-06-19&businessNumber=2618123567",
-    method: "GET",
-    success: function(response) {
-        $("#tableDiv").html(response);
-    },
-    error: function(xhr, status, error) {
-        // 오류 처리
-        console.error('오류가 발생했습니다. 관리자에게 문의하세요:', status, error);
-    }
-});
+// // parameter value
+// let urlParams = new URLSearchParams(window.location.search);
+// let dateParam = urlParams.get('date');
+//
+// console.log(dateParam);
+//
+// // calculate
+// $.ajax({
+//     url: "/partnerCalculate/receipt?date=" + dateParam + "&businessNumber=2618123567",
+//     method: "GET",
+//     success: function(response) {
+//         $("#tableDiv").html(response);
+//     },
+//     error: function(xhr, status, error) {
+//         // error handling
+//         console.error('오류가 발생했습니다. 관리자에게 문의하세요:', status, error);
+//     }
+// });
+// parameter value
+let urlParams = new URLSearchParams(window.location.search);
+let dateParam = urlParams.get('date');
 
+console.log(dateParam);
 
-$(document).ready(function() {
-    $('td.tg-mkvm').click(function() {
-        var businessNumber = $(this).siblings('.tg-mkvm.businessNumber').text();
-        var url = 'http://localhost/partnerCalculate/receipt?date=2023-06-19&businessNumber=' + encodeURIComponent(businessNumber);
+// td 클릭 이벤트 핸들러
+$('td.tg-mkvm').click(function() {
+    // 선택한 td의 사업자 번호 가져오기
+    let businessNumber = $(this).closest('tr').find('.tg-mkvm:nth-child(2)').text();
 
-        // AJAX 요청 또는 페이지 전환을 수행하는 코드 작성
-        // 예시: AJAX 요청을 수행하여 데이터를 가져오는 경우
-        $.ajax({
-            url: url,
-            type: 'GET',
-            dataType: 'html',
-            success: function(response) {
-                // AJAX 요청이 성공했을 때의 동작을 작성
-                // response 변수에는 서버에서 반환한 데이터가 포함됩니다.
-                // 가져온 데이터를 사용하여 필요한 동작을 수행하십시오.
-            },
-            error: function(xhr, status, error) {
-                // AJAX 요청이 실패했을 때의 동작을 작성
-                // 실패에 대한 처리를 수행하십시오.
-            }
-        });
-
-        // 페이지 전환을 수행하는 경우
-        // window.location.href = url;
+    // calculate
+    $.ajax({
+        url: "/partnerCalculate/receipt?date=" + dateParam + "&businessNumber=" + businessNumber,
+        method: "GET",
+        success: function(response) {
+            $("#tableDiv").html(response);
+        },
+        error: function(xhr, status, error) {
+            // error handling
+            console.error('오류가 발생했습니다. 관리자에게 문의하세요:', status, error);
+        }
     });
 });
+
