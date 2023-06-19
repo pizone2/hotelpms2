5b882c2ba72055d5a97b7ba8ef35d560a1616173
dongdongdongk <dhk9309@gmail.com>
2023-06-19 11:01:15 +0900

    2023-06-19(1차 완료)kdh

--- src/main/java/com/dev/pms/scheduler/Scheduler.java
+++ src/main/java/com/dev/pms/scheduler/Scheduler.java
@@ -1,15 +1,33 @@
 package com.dev.pms.scheduler;
 
+import com.dev.pms.stock.ManagerStockVO;
+import com.dev.pms.stock.PartnerManagerStockService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.swing.*;
+@Slf4j
 @Component
 public class Scheduler {
 
-    @Scheduled(cron = "*/3 * * * * *")
+    private PartnerManagerStockService partnerManagerStockService;
+
+    public Scheduler(PartnerManagerStockService partnerManagerStockService) {
+        this.partnerManagerStockService = partnerManagerStockService;
+    }
+
+    @Scheduled(cron = "*/10 * * * * *")
     public void updateOrderCompleted() throws Exception {
+        ModelAndView mv = new ModelAndView();
+        ManagerStockVO managerStockVO = new ManagerStockVO();
+        int result = partnerManagerStockService.updateOrderCompleted(managerStockVO);
+
         System.out.println("테스트 테스트");
 
     }
+
+    // 페이지 새로고침
 
 }
