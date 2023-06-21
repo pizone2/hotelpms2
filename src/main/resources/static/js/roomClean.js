8daa93dc170387689d6a7de086f1027024645d5f
jinjinzala <jinjinzala@naver.com>
2023-06-21 15:30:40 +0900

    정산 내역서 꾸미기

--- src/main/resources/static/js/roomClean.js
+++ src/main/resources/static/js/roomClean.js
@@ -1,3 +1,24 @@
+function handlePageChange() {
+    let tdElements = document.getElementsByTagName('td'); // 모든 <td> 요소 선택
+
+    for (let i = 0; i < tdElements.length; i++) {
+        let tdElement = tdElements[i];
+        let textContent = tdElement.textContent.trim(); // <td>의 텍스트 내용 가져오기
+
+        if (textContent === '확인') {
+            tdElement.id = 'ok'; // 확인인 td에 id 할당
+            tdElement.classList.add('p-3', 'border', 'mb-3', 'text-primary', 'fw-bolder'); // 확인인 td에 클래스 추가
+        }
+    }
+}
+
+$(document).ready(function() {
+    // 페이지 로드 시 실행
+    handlePageChange();
+
+    // 페이지 내 변경이 발생할 때마다 실행
+    $(document).on('DOMSubtreeModified', handlePageChange);
+});
 
 $(document).ready(function() {
     // roomNumber 클래스 클릭 시 모달 창 열기
@@ -8,6 +29,7 @@ $(document).ready(function() {
         var cleanNum = $(this).attr('data-cleanNum');
         openModal(note,savedName,cleanNum);
     });
+
 
     // openModal 함수 정의
     function openModal(note,savedName,cleanNum) {
@@ -27,9 +49,6 @@ $(document).ready(function() {
                 alert("실패")
             }
         });
-
-
-
 
 
         $('#remarks').val(note); // 품목명 입력 태그
@@ -60,7 +79,7 @@ window.addEventListener('DOMContentLoaded', event => {
         $('a.datatable-sorter').removeClass('datatable-sorter');
     });
 
-    // td에 id 할당하기
+    /*// td에 id 할당하기
     let tdElements = document.getElementsByTagName('td'); // 모든 <td> 요소 선택
 
     for (let i = 0; i < tdElements.length; i++) {
@@ -71,5 +90,5 @@ window.addEventListener('DOMContentLoaded', event => {
             tdElement.id = 'ok'; // 확인인 td에 id 할당
             tdElement.classList.add('p-3', 'border', 'mb-3', 'text-primary','fw-bolder'); // 확인인 td에 클래스 추가
         }
-    }
+    }*/
 });
